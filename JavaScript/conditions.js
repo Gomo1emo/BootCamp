0015fba049291bd472ab8093c839144cfdc7f8f6
Gomo1emo <tshabalalagomolemo@gmail.com>
2024-04-04 09:02:12 +0200

    3rd Commit

--- JavaScript/conditions.js
+++ JavaScript/conditions.js
@@ -1,17 +1,17 @@
 const gpa = 50;
-var pass_mark = 70;
-//const exp = 70;
-//const dist = 85;
-//const avg = 60;
-//const fail = 49;
-//const dnw = 0;
+var pass_mark = 90;
+const exp = 70;
+const dist = 85;
+const avg = 60;
+const fail = 49;
+const dnw = 0;
 
 if(pass_mark >= gpa && pass_mark <= 69) {
     console.log("You passed")
-}else if(pass_mark <= 49) {
+}else if(pass_mark <= gpa) {
     console.log("Try again next semester")
-} else if(pass_mark >= 69 && pass_mark < 84 ) {
+} else if(pass_mark >= exp && pass_mark < dist ) {
     console.log("You have passed with Examption!!")
-} else if(pass_mark >= 85 && pass_mark <= 100 ) {
+} else if(pass_mark >= dist && pass_mark <= 100 ) {
     console.log("You have passed with Distinction!!")
 } 
