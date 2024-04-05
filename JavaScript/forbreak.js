4abfc8a9e3445340b9f5f3f8a4cbdc9aad2c8ddc
Gomo1emo <tshabalalagomolemo@gmail.com>
2024-04-05 09:29:14 +0200

    4th

---
+++ JavaScript/forbreak.js
@@ -0,0 +1,8 @@
+//Lets create a for.. loop with break statement to limit the increament
+for(i=1; i<=9; i++){
+//break condition
+if(i==70){
+    break;
+}
+console.log(i);
+}
---
+++ JavaScript/loops.js
@@ -0,0 +1,20 @@
+let courses = ["Javascript","Java","Angular","React"];//Pre-defined array of courses
+
+let i; //Counter
+let length = courses.length;//total number of courses (Array)
+let faculty = courses.toString; //Number
+let len = faculty.length;
+let space =" ";//home made space
+
+for(i=0; i<length; i++) {
+     courses[i] += space;
+
+
+}
+console.log(courses); 
+
+//Lets display text five times according to the number of days in a week
+const n = 5;
+ for(let i = 1; i<=n; i++){
+    console.log(`Lets code in Javascript ` + i + ' days of the week');
+ }
